6e89df44d379f513ab23606160fa7f6267478bd2
Ryan Sullivan <mrsulliv@gmail.com>
2022-06-06 10:26:21 -0700

    Use react environment variables

--- src/utility/socket.js
+++ src/utility/socket.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8080', { autoConnect: false });
+// const socket = io('http://localhost:8080', { autoConnect: false });
+const socket = io(process.env.REACT_APP_BACKEND, { autoConnect: false });
+
 socket.onAny((event, ...args) => {
   console.log('socket event: ', event, args);
 });
